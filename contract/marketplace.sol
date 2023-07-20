3fe99d985dc9591eed6e8f2e49166cfcf9c9300d
Walter Brian <133803737+WalterBrian@users.noreply.github.com>
2023-07-20 20:58:15 +0100

    Update marketplace.sol

--- contract/marketplace.sol
+++ contract/marketplace.sol
@@ -2,8 +2,11 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-//nsert the interface of an ERC-20 token so your contract can interact with it.
-
+/**
+ * @title Marketplace Contract
+ * @notice This contract allows users to create, update, and remove products in a marketplace.
+ * @dev The contract interacts with an ERC-20 token to handle payments.
+ */
 interface IERC20Token {
   function transfer(address, uint256) external returns (bool);
   function approve(address, uint256) external returns (bool);
@@ -16,7 +19,9 @@ interface IERC20Token {
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-
+/**
+ * @title Marketplace Contract
+*/
 contract Marketplace {
     
     //in this section, we will optimise our contract
@@ -39,6 +44,9 @@ contract Marketplace {
 
     mapping (uint => Product) internal products;
 
+ /**
+  * @dev Event emitted when a new product is added to the marketplace
+*/
     event ProductAdded(
         uint indexed index,
         address owner,
@@ -49,6 +57,9 @@ contract Marketplace {
         uint price
     );
 
+/**
+ * @dev Event emitted when a product is updated
+*/
     event ProductUpdated(
         uint indexed index,
         string name,
@@ -58,6 +69,9 @@ contract Marketplace {
         uint price
     );
 
+/**
+ * @dev Event emitted when a product is removed from the marketplace
+*/
     event ProductRemoved(uint indexed index);
 
     modifier onlyProductOwner(uint _index) {
@@ -66,6 +80,16 @@ contract Marketplace {
         _;
     }
 
+
+/**
+     * @notice Update an existing product in the marketplace
+     * @param _index The index of the product to update
+     * @param _name The new name of the product
+     * @param _image The new image URL of the product
+     * @param _description The new description of the product
+     * @param _location The new location of the product
+     * @param _price The new price of the product
+*/
     function updateProduct(
         uint _index,
         string memory _name,
@@ -103,6 +127,11 @@ contract Marketplace {
 
     }
 
+
+/**
+     * @notice Remove a product from the marketplace
+     * @param _index The index of the product to remove
+*/
     function removeProduct(uint _index) public onlyProductOwner(_index) {
         // Get the product from the mapping
         Product storage product = products[_index];
@@ -115,11 +144,22 @@ contract Marketplace {
         emit ProductRemoved(_index);
     }
 
-    //we need to adapt our writeProduct function
-    //When a user adds a new product to your marketplace contract, you set _sold to the value 0
-    //because it tracks the number of times the product was sold
-    //this is initially always zero, and therefore you don't need a parameter.
-
+/**
+ * @dev Add a new product to the marketplace.
+ * @param _name The name of the product.
+ * @param _image The URL of the product image.
+ * @param _description The description of the product.
+ * @param _location The location of the product.
+ * @param _price The price of the product in cUSD.
+ * @notice This function allows users to add a new product to the marketplace.
+ * @dev Only the product owner can call this function.
+ * @dev The product name must not be empty and should be less than or equal to 100 characters.
+ * @dev The product image URL must not be empty.
+ * @dev The product description must not be empty.
+ * @dev The product location must not be empty.
+ * @dev The product price must be greater than zero.
+ * @return The index of the newly added product.
+*/
     function writeProduct(
         string memory _name,
         string memory _image, //örnek: orange.png
@@ -157,7 +197,19 @@ contract Marketplace {
         
     }
 
-    //we need to change the readProduct function
+/**
+ * @dev Read the details of a product from the marketplace.
+ * @param _index The index of the product to read.
+ * @notice This function allows users to read the details of a product from the marketplace.
+ * @dev The product index must be valid.
+ * @return The address of the product owner.
+ * @return The name of the product.
+ * @return The URL of the product image.
+ * @return The description of the product.
+ * @return The location of the product.
+ * @return The price of the product in cUSD.
+ * @return The number of times the product has been sold.
+*/
     function readProduct(uint _index) public view returns (
         address payable,
         string memory,
@@ -179,7 +231,15 @@ contract Marketplace {
         );
     }
 
-    //we need to create a function to buy products from our contract.
+/**
+ * @dev Buy a product from the marketplace.
+ * @param _index The index of the product to buy.
+ * @notice This function allows users to buy a product from the marketplace.
+ * @dev The product index must be valid.
+ * @dev The buyer must send enough funds to purchase the product.
+ * @dev The product owner will receive the payment in cUSD tokens.
+ * @dev The product's sold count will be incremented.
+*/
     function buyProduct(uint _index) public payable {
 
     require(_index < productsLength, "Invalid product index");
@@ -218,7 +278,11 @@ contract Marketplace {
 }
 
 
-    //create a public function to return the number of products stored
+/**
+ * @dev Get the number of products stored in the marketplace.
+ * @notice This function returns the number of products currently stored in the marketplace.
+ * @return The number of products stored in the marketplace.
+*/
     function getProductsLength() public view returns (uint) {
         return productsLength;
     }
